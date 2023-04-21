3a196e1ca4eb22ed014170f804a8a42eb294b034
Shilol Pesahov <pesahov777@gmail.com>
2023-04-21 09:01:02 +0300

    listener

--- src/test/java/tests/SearchCarTests.java
+++ src/test/java/tests/SearchCarTests.java
@@ -1,12 +1,28 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class SearchCarTests extends TestBase{
 
-    public void searchCurrentSuccess(){
-        app.getHelperCar().searchCurrentMonth("Tel Aviv, Israel","4/25/2023","4/28/2023");
+    @Test
+    public void searchCurrentMonthSuccess(){
+        app.getHelperCar().searchCurrentMonth("Tel Aviv, Israel","4/27/2023","4/28/2023");
         app.getHelperCar().submit();
-        //Assert.assertTrue(app.getHelperCar().isListOfCarsAppeared());
+        Assert.assertTrue(app.getHelperCar().isListOfCarsAppeared());
     }
+
+    @Test
+    public void searchCurrentYearSuccess(){
+        app.getHelperCar().searchCurrentYear("Tel Aviv, Israel","6/30/2023","10/28/2023");
+        app.getHelperCar().submit();
+        Assert.assertTrue(app.getHelperCar().isListOfCarsAppeared());
+    }
+    @Test
+    public void searchAnyPeriodSuccess(){
+        //app.getHelperCar().searchAnyPeriod("Tel Aviv, Israel","12/27/2023","2/28/2024");
+        app.getHelperCar().submit();
+        Assert.assertTrue(app.getHelperCar().isListOfCarsAppeared());
+    }
+
 }
